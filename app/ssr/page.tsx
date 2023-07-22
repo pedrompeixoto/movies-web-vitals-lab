8f5a96e7a9bd59a47f1038612da4f56d07a1da5e
pedrompeixoto <pmpeixoto@protonmail.com>
2023-07-22 16:33:24 +0100

    SSR display a movie list.

--- app/ssr/page.tsx
+++ app/ssr/page.tsx
@@ -1,4 +1,5 @@
 
+import MovieEntry from "@/components/movie-entry";
 import { getMoviesAPISingleton } from "../../movies-api/movies-api"
 
 export const dynamic = "force-dynamic"
@@ -10,8 +11,17 @@ export default async function SSR() {
   const movies = await moviesAPI.searchByTitle(movieTitleQuery);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>{ movies.results[0].titleText.text }</p>
-    </main>
+    <div className="flex flex-col items-center p-24 gap-10">
+      <div className="grid grid-cols-4 gap-8 place-content-center">
+        { movies.results.map(movie => {
+        {/* TODO placeholder when image is missing. Entries without a poster are hidden for now */} 
+          if (movie.primaryImage?.url) {
+            return (<MovieEntry key={movie._id} movie={movie}></MovieEntry>);
+          } else {
+            return null;
+          }
+        })}
+      </div>
+    </div>
   )
 }
